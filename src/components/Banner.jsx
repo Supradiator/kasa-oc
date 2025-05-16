9962b0d34b19da136a2846efef9592043e17cbfd
Supradiator <trailax00@gmail.com>
2025-05-16 10:46:52 +0200

    banner : img, txt, opcity -> props

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,19 @@
 import '../css/Banner.css'
 import '../css/Global.css'
 
-function Banner() {
-    return (
-        <div className="banner container">
-            <h1>Chez vous, partout et ailleurs</h1>
-        </div>
-    )
+function Banner({ text, image, overlayOpacity = 0.6 }) {
+  const bannerStyle = {
+    backgroundImage: `
+      linear-gradient(rgba(0, 0, 0, ${overlayOpacity}), rgba(0, 0, 0, ${overlayOpacity})),
+      url(${image})
+    `,
+  }
+
+  return (
+    <div className="banner container" style={bannerStyle}>
+      {text && <h1>{text}</h1>}
+    </div>
+  )
 }
 
 export default Banner
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,11 @@ import Cards from '../components/Cards'
 function Home() {
     return (
       <div>
-        <Banner />
+        <Banner 
+          text="Chez vous, partout et ailleurs"
+          image="/src/assets/banner-home.svg"
+          overlayOpacity={0.6}
+        />
         <Cards />
       </div>
     )
